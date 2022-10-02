42cb792ea92f506644c799d947c3af1cae93d78d
cooper <pbg0205@naver.com>
2022-10-03 00:28:52 +0900

    first commit

---
+++ src/main/java/com/cooper/transactionalpractice/TransactionalPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.cooper.transactionalpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionalPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cooper/transactionalpractice/TransactionalPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooper.transactionalpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionalPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
